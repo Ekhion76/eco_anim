cdf50c18bcefa23b19dc6f88237528d32d8cf5b5
Ekhion76 <76602492+Ekhion76@users.noreply.github.com>
2023-12-17 21:04:55 +0100

    Delete empty lines

--- client/main.lua
+++ client/main.lua
@@ -1,3 +1,6 @@
+function print_r(data)
+    print(json.encode(data, {indent=true}))
+end
 local _PlayerPedId, cam
 local peds = {
     { hash = 'a_m_y_skater_01', flag = 16 },
@@ -6,7 +9,6 @@ local peds = {
 }
 
 RegisterCommand('anim', function()
-
     SetNuiFocus(true, true)
     SendNUIMessage({
         subject = 'OPEN'
@@ -14,29 +16,22 @@ RegisterCommand('anim', function()
 end)
 
 function loadAnimDict(data)
-
     if not DoesAnimDictExist(data.dict) then
         return false
     end
 
     if not HasAnimDictLoaded(data.dict) then
-
         RequestAnimDict(data.dict)
-
         while not HasAnimDictLoaded(data.dict) do
             Wait(10)
         end
     end
-
     return true
 end
 
 function camRender()
-
     if not cam then
-
         local pos = GetEntityForwardVector(peds[2].npc) * 5 + GetEntityCoords(peds[2].npc)
-
         local curCam = GetRenderingCam()
         cam = CreateCamWithParams("DEFAULT_SCRIPTED_CAMERA", pos.x, pos.y, pos.z + 0.5, -5.0, 0.0, peds[2].heading - 180, 60.00, false, 0)
         SetCamActive(cam, true)
@@ -46,15 +41,10 @@ function camRender()
 end
 
 RegisterNUICallback('playAnim', function(data, cb)
-
     _PlayerPedId = PlayerPedId()
-
     stopAnim()
-
     ClearPedTasksImmediately(_PlayerPedId)
-
     if not loadAnimDict(data) then
-
         cb({
             loadAnim = false,
             dict = data.dict,
@@ -76,9 +66,7 @@ RegisterNUICallback('playAnim', function(data, cb)
 end)
 
 function modelLoader(model)
-
     if not HasModelLoaded(model) then
-
         RequestModel(model)
         Wait(100)
         while not HasModelLoaded(model) do
@@ -106,9 +94,7 @@ function npcHandler(lib, anim)
     forward.y = math.cos(math.rad(heading + 90.0))
 
     for k, param in pairs(peds) do
-
         if not param.npc then
-
             modelLoader(param.hash)
 
             x = pos.x - forward.x * (k - 2) * xDistance + offset.x
@@ -116,12 +102,10 @@ function npcHandler(lib, anim)
 
             local _, z = GetGroundZFor_3dCoord(x, y, pos.z, 0)
             param.npc = CreatePed(6, param.hash, x, y, z, npcHeading, false, false)
-
             param.pos = vector3(x, y, z)
             param.heading = npcHeading
             TaskPlayAnim(param.npc, lib, anim, 8.0, 8.0, -1, param.flag, 0, false, false, false)
         else
-
             SetEntityCoords(param.npc, param.pos)
             SetEntityHeading(param.npc, param.heading)
             TaskPlayAnim(param.npc, lib, anim, 8.0, 8.0, -1, param.flag, 0, false, false, false)
@@ -130,27 +114,21 @@ function npcHandler(lib, anim)
 end
 
 function npcDeleter()
-
     for _, params in pairs(peds) do
-
         DeletePed(params.npc)
         params.npc = nil
     end
 end
 
 function stopAnim()
-
     for _, param in pairs(peds) do
-
         if param.npc then
-
             ClearPedTasksImmediately(param.npc)
         end
     end
 end
 
 RegisterNUICallback('exit', function(data, cb)
-
     SetCamActive(cam, false)
     DestroyCam(cam, true)
     RenderScriptCams(false, 1, 1000, 1, 1);
